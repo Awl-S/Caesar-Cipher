8703a98a75e23d7a9195c9bf435619ab9ea060d8
Awl-s <santanet@bk.ru>
2024-11-13 17:56:33 +0700

    Update .gitignore to ignore build and vscode directories

--- build/CMakeFiles/Caesar-Cipher_C.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Caesar-Cipher_C.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/memory_check_c.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/memory_check_c"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/memory_check_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
